9e2308a566f20ce4dde94933eff7036521446e5f
Burak Duğancı <burakduganci@gmail.com>
2018-07-16 16:07:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+String.prototype.turkishtoEnglish = function () {
+    return this.replace(/[ş]+/g,'s')
+        .replace(/[Ş]+/g,'S')
+        .replace(/[Ü]+/g,'U')
+        .replace(/[ü]+/g,'u')
+        .replace(/[Ö]+/g,'O')
+        .replace(/[ö]+/g,'o')
+        .replace(/[İ]+/g,'I')
+        .replace(/[ı]+/g,'i')
+        .replace(/[ğ]+/g,'g')
+        .replace(/[Ğ]+/g,'G')
+        .replace(/[Ç]+/g,'C')
+        .replace(/[ç]+/g,'c')
+};
